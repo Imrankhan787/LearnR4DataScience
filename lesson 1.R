9478f4b901913c7339cda5361f0b54d067fc436c
Imrankhan787 <ikhan004@odu.edu>
2022-10-27 20:17:52 -0400

    please commit

---
+++ lesson 1.R
@@ -0,0 +1,8 @@
+
+##
+#git config --replace-all user.email "ikhan004@odu.edu"
+library(tidyverse)
+
+#git config --replace-all user.name "Imrankhan787"
+
+#>>> C:/Program Files/Git/bin/git.exe commit -F C:/Users/Imran787/AppData/Local/Temp/RtmpkDSbDq/git-commit-message-5b08307a474e.txt
